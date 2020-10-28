a008f9a8915d97f47d8e66f09e762c9f29e68094
Crystal Dennis <zayehalo@gmail.com>
2020-10-28 13:18:37 -0400

    django version of personal website to replace old website

---
+++ zayehalo_website/urls.py
@@ -0,0 +1,39 @@
+"""zayehalo_website URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/dev/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.contrib.auth import views as auth_views #for admin login
+from django.conf.urls import include
+from .views import index
+from django.conf.urls.static import static
+
+from . import views
+
+urlpatterns = [
+    path('', index),
+    path('admin/', admin.site.urls),
+    path('home', views.index, name="index"),
+    path('about', views.about, name="about"),
+    path('contact', views.contact, name="contact"),
+    path('accounts/profile',views.ProfileView.as_view(), name = 'profile'),
+
+    #Django auth--- use my accounts folder instead of django generated
+    path('accounts/login', auth_views.LoginView.as_view(template_name="accounts/login.html"), name='login'),
+    path('accounts/logout', auth_views.LogoutView.as_view(), name="logout"),
+] 
+
+# autmaticall install all the login appsVVVV
+# path('accounts/', include('django.contrib.auth.urls'))
